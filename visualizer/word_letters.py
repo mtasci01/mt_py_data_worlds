e029551a1196d026708b99a15a66a82acbedce97
Marco Tascini <tascinimarco@tiscali.it>
2025-08-03 21:07:41 +0200

    word_letters

---
+++ visualizer/word_letters.py
@@ -0,0 +1,21 @@
+from matplotlib import pyplot as plt
+
+
+alphabet = 'abcdefghijklmnopqrstuvwxyz'
+alphabet_map = {}
+
+for i in range(len(alphabet)):
+    alphabet_map[alphabet[i]] = i + 1
+
+word = "tarchies"
+word = word.lower()
+
+xs = []
+ys = []
+
+for i in range(len(word)):
+    ys.append(alphabet_map[word[i]])
+    xs.append(i + 1)
+
+plt.plot(xs,ys, color='blue')
+plt.show()   
